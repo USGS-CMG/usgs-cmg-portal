d3a8f283f9ce96e67f168e00cd8fc5f900f3b539
Kyle Wilcox <kyle@axiomalaska.com>
2015-07-22 17:54:19 -0400

    Add additional global attributes for retreval later on

--- woods_hole_obs_data/collect.py
+++ woods_hole_obs_data/collect.py
@@ -494,6 +494,9 @@ def main(output, download_folder, do_download, projects, csv_metadata_file, file
             file_global_attributes.update(global_attributes)
             file_global_attributes['id'] = station_id
             file_global_attributes['title'] = '{0} - {1}'.format(project_name, os.path.basename(down_file))
+            file_global_attributes['MOORING'] = mooring_id
+            file_global_attributes['original_filename'] = fname
+            file_global_attributes['original_folder'] = project_name
             if project_name in project_metadata:
                 for k, v in project_metadata[project_name].items():
                     if v and k.lower() not in ['id', 'title', 'catalog_xml', 'project_name']:
