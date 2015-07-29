036b588e9d94942ae7cb8039eae8101bfd6ec646
Yan, Andrew N <ayan@usgs.gov>
2015-07-29 15:02:48 -0500

    added template for sciwms user parameters

---
+++ catalog_harvest/sciwms_connect.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 29, 2015
+
+@author: ayan
+
+The file is a template for your sciwms
+user credentials. 
+
+NEVER COMMIT PASSWORDS TO GITHUB.
+
+'''
+
+SCIWMS_REST_URL = "the URL for sciwms's rest endpoint goes here"
+USER = "place your sciwms username here"
+PASSWORD = "place your sciwms password here"
