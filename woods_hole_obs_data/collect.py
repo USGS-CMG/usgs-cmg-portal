4a4e924c2b250175b9d95c250c00429a8934cb1f
Kyle Wilcox <kyle@axiomalaska.com>
2015-08-25 11:18:18 -0400

    Update regex to ignore abs-cal and abss-cal

--- woods_hole_obs_data/collect.py
+++ woods_hole_obs_data/collect.py
@@ -189,8 +189,8 @@ def download(folder, project_metadata, filesubset):
 
     try:
         for k, v in project_metadata.items():
-            # http://regexr.com/3bh4v
-            datasets = Crawl(v['catalog_xml'], select=['([0-9]+\..*|(?!.*var-).*(-(a|A)|(s|d|tide)-cal){1}(?!lp)(?!1(h|H))\.*.*)'], skip=skips).datasets
+            # http://regexr.com/3bleg
+            datasets = Crawl(v['catalog_xml'], select=['([0-9]+\..*|.*(-(a|A)|(ls|[0-9]s|isus|[aAB]s|sgs|aqds|vs|pcs|d|tide)-cal){1}(?!lp)(?!1(h|H))\.*.*)'], skip=skips).datasets
             logger.info("Found {0} datasets in {1}!".format(len(datasets), k))
             total_datasets += datasets
         logger.info("Found {0} TOTAL datasets!".format(len(total_datasets)))
