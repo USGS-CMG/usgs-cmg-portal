1f9a7dda248bf6fb064fcbb69b85296cd864dd1f
Rich Signell <rsignell@usgs.gov>
2016-08-18 13:08:44 -0400

    Update get_daily_iso.py

--- catalog_harvest/get_daily_iso.py
+++ catalog_harvest/get_daily_iso.py
@@ -25,7 +25,8 @@ SAVE_DIR="/usgs/data0/iso/iso_records"
 THREDDS_SERVERS = {
     "necofs1": "http://www.smast.umassd.edu:8080/thredds/forecasts.html",
     "coawst":   "http://geoport-dev.whoi.edu/thredds/catalog/coawst_4/use/fmrc/catalog.html",
-    "estofs_wh": "http://geoport-dev.whoi.edu/thredds/estofs_agg.html"
+    "estofs_wh": "http://geoport-dev.whoi.edu/thredds/estofs_agg.html",
+    "pacioos": "http://oos.soest.hawaii.edu/thredds/idd/ocn_mod.html"
 }
 
 metadata_files = get_metadata(thredds_servers=THREDDS_SERVERS,
