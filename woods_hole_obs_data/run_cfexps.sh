9027c5a9f2d98df7ac735d8c98fe5dfe8a4fcd9f
Kyle Wilcox <kyle@axiomalaska.com>
2016-12-02 15:21:29 -0500

    Reorder and add missing experiements to bash script

--- woods_hole_obs_data/run_cfexps.sh
+++ woods_hole_obs_data/run_cfexps.sh
@@ -9,12 +9,16 @@
 #
 python collect.py -p ARGO_MERCHANT --folder download -o ../../CF-1.6new
 python collect.py -p BARNEGAT --folder download -o ../../CF-1.6new
+python collect.py -p BARNEGAT_2014 --folder download -o ../../CF-1.6new
 python collect.py -p BUZZ_BAY --folder download -o ../../CF-1.6new
 python collect.py -p BW2011 --folder download -o ../../CF-1.6new
+python collect.py -p CALCSTWET --folder download -o ../../CF-1.6new
 python collect.py -p CAMP --folder download -o ../../CF-1.6new
 python collect.py -p CAPE_COD_BAY --folder download -o ../../CF-1.6new
 python collect.py -p CC_MISC --folder download -o ../../CF-1.6new
-python collect.py -p GOMEX_CI --folder download -o ../../CF-1.6new
+python collect.py -p CHANDELEUR_13 --folder download -o ../../CF-1.6new
+python collect.py -p CHINCOTEAGUE --folder download -o ../../CF-1.6new
+python collect.py -p DAUPHIN --folder download -o ../../CF-1.6new
 python collect.py -p DEEP_REEF --folder download -o ../../CF-1.6new
 python collect.py -p DIAMONDSHOALS --folder download -o ../../CF-1.6new
 python collect.py -p DWDS_106 --folder download -o ../../CF-1.6new
@@ -27,6 +31,7 @@ python collect.py -p FI14 --folder download -o ../../CF-1.6new
 python collect.py -p GB_SED --folder download -o ../../CF-1.6new
 python collect.py -p GLOBEC_GB --folder download -o ../../CF-1.6new
 python collect.py -p GLOBEC_GSC --folder download -o ../../CF-1.6new
+python collect.py -p GOMEX_CI --folder download -o ../../CF-1.6new
 python collect.py -p GULF_MAINE --folder download -o ../../CF-1.6new
 python collect.py -p HUDSON_SVALLEY --folder download -o ../../CF-1.6new
 python collect.py -p HURRIRENE_bb --folder download -o ../../CF-1.6new
@@ -44,8 +49,9 @@ python collect.py -p MOBILE_BAY --folder download -o ../../CF-1.6new
 python collect.py -p MONTEREY_BAY --folder download -o ../../CF-1.6new
 python collect.py -p MONTEREY_CAN --folder download -o ../../CF-1.6new
 python collect.py -p MVCO_11 --folder download -o ../../CF-1.6new
+python collect.py -p MVCO_14 --folder download -o ../../CF-1.6new
 python collect.py -p MYRTLEBEACH --folder download -o ../../CF-1.6new
-python collect.py -p NE_SLOpE --folder download -o ../../CF-1.6new
+python collect.py -p NE_SLOPE --folder download -o ../../CF-1.6new
 python collect.py -p NEARSHORE --folder download -o ../../CF-1.6new
 python collect.py -p OCEANOG_C --folder download -o ../../CF-1.6new
 python collect.py -p ORANGE_COUNTY --folder download -o ../../CF-1.6new
@@ -53,12 +59,10 @@ python collect.py -p PONCHARTRAIN --folder download -o ../../CF-1.6new
 python collect.py -p PV_SHELF --folder download -o ../../CF-1.6new
 python collect.py -p PV_SHELF04 --folder download -o ../../CF-1.6new
 python collect.py -p PV_SHELF07 --folder download -o ../../CF-1.6new
+python collect.py -p RCNWR --folder download -o ../../CF-1.6new
 python collect.py -p SAB_SED --folder download -o ../../CF-1.6new
+python collect.py -p SANDWICH2016 --folder download -o ../../CF-1.6new
 python collect.py -p SOUTHERN_CAL --folder download -o ../../CF-1.6new
 python collect.py -p STRESS --folder download -o ../../CF-1.6new
 python collect.py -p WFAL --folder download -o ../../CF-1.6new
 python collect.py -p WRIGHTSVILLE --folder download -o ../../CF-1.6new
-python collect.py -p CHANDELEUR_13 --folder download -o ../../CF-1.6new
-python collect.py -p DAUPHIN --folder download -o ../../CF-1.6new
-python collect.py -p RCNWR --folder download -o ../../CF-1.6new
-
